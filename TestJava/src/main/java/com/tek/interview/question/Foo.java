8a1a2e45207b19d40f3785a72882da4c55298fae
v4vamshi <vamshi@192.168.0.16>
2017-01-22 14:32:02 -0500

    Refactor..

--- TestJava/src/main/java/com/tek/interview/question/Foo.java
+++ TestJava/src/main/java/com/tek/interview/question/Foo.java
@@ -35,62 +35,6 @@ Sum of orders: 153.81
  
 ******************************************************************************************** */
 
-/*
- * represents an item, contains a price and a description.
- *
- */
-class Item {
-
-	private String description;
-	private float price;
-
-	public Item(String description, float price) {
-		super();
-		this.description = description;
-		this.price = price;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public float getPrice() {
-		return price;
-	}
-}
-
-/*
- * represents an order line which contains the @link Item and the quantity.
- *
- */
-class OrderLine {
-
-	private int quantity;
-	private Item item;
-
-	/*
-	 * @param item Item of the order
-	 * 
-	 * @param quantity Quantity of the item
-	 */
-	public OrderLine(Item item, int quantity) throws Exception {
-		if (item == null) {
-			System.err.println("ERROR - Item is NULL");
-			throw new Exception("Item is NULL");
-		}
-		assert quantity > 0;
-		this.item = item;
-		this.quantity = quantity;
-	}
-
-	public Item getItem() {
-		return item;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-}
 
 class Order {
 
---
+++ TestJava/src/main/java/com/tek/interview/question/Item.java
@@ -0,0 +1,24 @@
+package com.tek.interview.question;
+/*
+ * represents an item, contains a price and a description.
+ *
+ */
+class Item {
+
+	private String description;
+	private float price;
+
+	public Item(String description, float price) {
+		super();
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+}
